64ab2e80ca4bb0cc63eb5fd20bb648058bb240e5
SANDIYAVENKATESH <sav49@pitt.edu>
2016-09-25 22:01:52 -0400

    Completed chapter 2 exercise 3

--- Ch2-Exercises/exercise3.py
+++ Ch2-Exercises/exercise3.py
@@ -1,2 +1,19 @@
-#
-# name: Sandiya Venkatesh
+
+#############################################
+# name  : Sandiya Venkatesh
+# email : SAV49@pitt.edu
+# date  : Sep 25 2016
+# class : CS0008-f2016
+# instructor : Max Novelli (man8@pitt.edu)
+##############################################
+# Description:
+# Starting with Python, Chapter 2, Exercise 3
+# Notes :
+
+# PROGRAM START ###############################
+total_sq_ft = float(input('enter the total square feet in a track of land?'))
+nr_of_acres = float(total_sq_ft / 43560)
+nr_of_acres = format(nr_of_acres,'.2f')
+print('the number of acres in the track =',nr_of_acres)
+# PROGRAM END #################################
+
