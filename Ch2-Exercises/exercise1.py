b88dbbdbffb7af6ceeccf4130d3b9de4e793b108
SANDIYAVENKATESH <sav49@pitt.edu>
2016-11-28 00:51:29 -0500

    This is the final version of the assignment.

---
+++ Ch2-Exercises/exercise1.py
@@ -0,0 +1,21 @@
+#############################################
+# name  : Sandiya Venkatesh
+# email : SAV49@pitt.edu
+# date  : Oct 26 2016
+# class : CS0008-f2016
+# instructor : Max Novelli (man8@pitt.edu)
+##############################################
+# Description:
+# Starting with Python, Chapter 2, Exercise 1
+# Notes:
+###############################################
+
+# PROGRAM START ###############################
+
+print('Sandiya Venkatesh')
+print('33 Sherwood Drive, Mountain Lakes, New Jersey, 07046')
+print('9738300419')
+print('Computer Science')
+
+# PROGRAM END #################################
+
---
+++ Ch2-Exercises/exercise2.py
@@ -0,0 +1,21 @@
+#############################################
+# name  : Sandiya Venkatesh
+# email : SAV49@pitt.edu
+# date  : Oct 26 2016
+# class : CS0008-f2016
+# instructor : Max Novelli (man8@pitt.edu)
+##############################################
+# Description:
+# Starting with Python, Chapter 2, Exercise 2
+# Notes:
+###############################################
+
+# PROGRAM START ###############################
+
+sales_amount = float(input('Enter Sales Amount ='))
+sales_profit = float(sales_amount * .23)
+sales_profit = format(sales_profit,'.2f')
+print('Sales Profit =',sales_profit)
+
+
+# PROGRAM END #################################
